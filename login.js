169e4973bda0d9868d91e1bb7e6ab3d04f6c22d4
Fewlateo <fillatio2543@gmail.com>
2021-10-01 14:57:45 +0700

    week8

---
+++ login.js
@@ -0,0 +1,6 @@
+// 1.include the package
+var mysql = require("mysql");
+var express = require("myexpress");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var path = require("path");
